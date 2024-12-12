4ba513d4c0de2b2892f2880b384b3690fe93d90f
marcopeg <marco.pegoraro@gmail.com>
2024-12-12 15:32:38 +0100

    Update router

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -5,23 +5,43 @@ import "material-icons/iconfont/material-icons.css";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { MuiProvider } from "./providers/MuiProvider/index.ts";
 import { AuthProvider } from "./providers/AuthProvider";
 import { ConnectionProvider } from "./providers/ConnectionProvider";
 import { App } from "./App.tsx";
 import "./i18n.ts";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <MuiProvider>
-      <Router>
+// Wrap `AppRoutes` within the providers in the router definition
+const router = createBrowserRouter(
+  [
+    {
+      path: "/*", // Allow sub-routes to match
+      element: (
         <AuthProvider>
           <ConnectionProvider>
             <App />
           </ConnectionProvider>
         </AuthProvider>
-      </Router>
+      ),
+    },
+  ],
+  {
+    future: {
+      v7_startTransition: true, // Enables future flag
+      v7_relativeSplatPath: true, // Enables future flag
+      v7_fetcherPersist: true, // Enables future flag
+      v7_normalizeFormMethod: true, // Enables future flag
+      v7_partialHydration: true, // Enables future flag
+      v7_skipActionErrorRevalidation: true, // Enables future flag
+    },
+  }
+);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <MuiProvider>
+      <RouterProvider router={router} />
     </MuiProvider>
   </StrictMode>
 );

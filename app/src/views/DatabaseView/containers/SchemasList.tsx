ea220bfff0ab47f9face4980b9233c3b5f3ba3b8
marcopeg <marco.pegoraro@gmail.com>
2024-12-29 23:05:13 +0100

    Show schemas

--- app/src/views/DatabaseView/containers/SchemasList.tsx
+++ app/src/views/DatabaseView/containers/SchemasList.tsx
@@ -1,3 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  ListItemIcon,
+  Divider,
+  Icon,
+} from "@mui/material";
 import { Connection } from "../../../providers/ConnectionProvider";
 import { useSchemas } from "../hooks/use-schemas";
 
@@ -7,6 +18,29 @@ interface SchemasListProps {
 
 export const SchemasList: React.FC<SchemasListProps> = ({ conn }) => {
   const { items } = useSchemas(conn);
-  console.log(items);
-  return null;
+
+  return (
+    <List>
+      {items.map((schema) => (
+        <React.Fragment key={schema.schema_name}>
+          <ListItem disablePadding disableGutters>
+            <ListItemButton
+              component={Link}
+              to={`/${conn.name}/${conn.database}/${schema.schema_name}`}
+            >
+              <ListItemIcon>
+                <Icon>folder</Icon>
+              </ListItemIcon>
+              <ListItemText
+                primary={schema.schema_name}
+                secondary={`Size: ${schema.total_size_readable}, ${schema.tables_count} tables`}
+              />
+              <Icon>chevron_right</Icon>
+            </ListItemButton>
+          </ListItem>
+          <Divider />
+        </React.Fragment>
+      ))}
+    </List>
+  );
 };
